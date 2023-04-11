d5619c54776d065a72e347c14192e8731ab8086a
Amir Aliev <amir1uke@mail.ru>
2023-04-11 04:35:08 +0400

    1

---
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -0,0 +1,46 @@
+package web.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+@Repository
+public class UserDAOImpl implements UserDAO{
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public UserDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public User getUserById(Integer id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void deleteUser(Integer id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+    }
+}
