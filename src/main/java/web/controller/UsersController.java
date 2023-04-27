e79b47f5ea95e6c6870173e3e72793f2a1be6634
Amir Aliev <amir1uke@mail.ru>
2023-04-28 01:47:20 +0400

    final2

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -10,17 +10,8 @@ import java.util.List;
 
 
 @Controller
-//@RequestMapping("/users")
+@RequestMapping()
 public class UsersController {
-//    @GetMapping
-//    public String printWelcome(ModelMap model) {
-//        List<String> messages = new ArrayList<>();
-//        messages.add("Hello!");
-//        messages.add("I'm Spring MVC application");
-//        messages.add("5.2.0 version by sep'19 ");
-//        model.addAttribute("messages", messages);
-//        return "userlist";
-//    }
     private final UserService userService;
 
     public UsersController(UserService userService) {
@@ -43,19 +34,19 @@ public class UsersController {
         userService.add(user);
         return "redirect:/users";
     }
-    @GetMapping("user-delete/{id}")
+    @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
-        userService.deleteUser(id);
-        return "redirect:/users";
-    }
+    userService.deleteUser(id);
+    return "redirect:/users";
+}
     @GetMapping("/user-update/{id}")
     public String updateUserForm(@PathVariable("id") Long id,Model model) {
         User user = userService.getUserById(id);
         model.addAttribute("user",user);
         return "/user-update";
     }
-    @PostMapping("/user-update")
-    public String updateUser(User user) {
+    @PatchMapping("/user-update/{id}")
+    public String updateUser(@ModelAttribute("user") User user) {
         userService.updateUser(user);
         return "redirect:/users";
     }
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -7,34 +7,29 @@ import web.model.User;
 
 import java.util.List;
 @Service
-
+@Transactional
 public class UserServiceImpl implements UserService {
     private final UserDAO userDAO;
 
     public UserServiceImpl(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
-    @Transactional
     @Override
     public void add(User user) {
         userDAO.add(user);
     }
-    @Transactional
     @Override
     public User getUserById(Long id) {
         return userDAO.getUserById(id);
     }
-    @Transactional
     @Override
     public List<User> listUsers() {
         return userDAO.listUsers();
     }
-    @Transactional
     @Override
     public void updateUser(User user) {
         userDAO.updateUser(user);
     }
-    @Transactional
     @Override
     public void deleteUser(Long id) {
         userDAO.deleteUser(id);
